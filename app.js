9074832555b3e10407ce4f251c79fc660323a01b
cygomezca <75762510+cygomezca@users.noreply.github.com>
2021-08-11 10:04:11 -0500

    10
    ggfsd

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 app.use(
     session({
-    secret: "mysecretapp",
+    secret: "secret",
     resave: true,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: config.MONGODB_URI }),
